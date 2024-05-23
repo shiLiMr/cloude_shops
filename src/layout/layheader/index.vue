6b8b1e3ff7ea19324a28b7e6502e1723f067f6eb
shilihaoa <lhxhl0303.163.com>
2024-05-23 09:31:56 +0800

    修复动态路由删除不了Bug

--- src/layout/layheader/index.vue
+++ src/layout/layheader/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <el-header >
+    <div class="header-header">
+        <el-header>
             <div class="header-left">
                 <svg-icon class="svgicons" @click="store.isCollapse = !store.isCollapse" size="20px"
                     :icon="store.isCollapse ? 'Expand' : 'Fold'"></svg-icon>
@@ -27,7 +27,7 @@
                 </el-dropdown>
             </div>
         </el-header>
-        <div class="m">
+        <div class="m" v-if="editableTabs.length > 0">
             <el-tabs v-model="$route.path" type="card" class="demo-tabs" editable @tab-remove="handleTabsEdit"
                 @tab-change="once">
                 <el-tab-pane v-for="item in editableTabs" :key="item.name" :label="item.title" :name="item.name">
@@ -41,24 +41,16 @@
 import { ref, watch, watchEffect, } from 'vue'
 import { useUserInforStore } from '@/stores/userinfor'
 const store = useUserInforStore()
-import { useRoute, onBeforeRouteUpdate ,useRouter} from 'vue-router';
+import { useRoute, onBeforeRouteUpdate, useRouter } from 'vue-router';
 const route = useRoute()
 const router = useRouter()
 console.log(route);
 
+const editableTabs = ref<any>([])
 
 const isList = ref<any>([]) // 面包屑数据
 watch(route, (newValue, oldValue) => {
     isList.value = newValue.matched.filter(item => item.meta.title)
-}, {
-    deep: true,
-    immediate: true,
-}
-)
-
-// 监听路由变化
-const editableTabs = ref<any>([])
-watchEffect(()=>{
     let flag = editableTabs.value.some((item: any) => item.name == route.path)
     if (!flag) {
         editableTabs.value.push({
@@ -66,17 +58,39 @@ watchEffect(()=>{
             name: route.path,
         })
     }
-})
-// onBeforeRouteUpdate((to, from) => { // 路由切换触发函数
-//     console.log(to);
-// })
-const once = (name:string) => {// 点击 跳转页面
-    console.log(name);
-    router.push({path:name})
+}, {
+    deep: true,
+    immediate: true,
 }
-const handleTabsEdit = (val:any) => {// 删除事件
+)
+
+// 监听路由变化
+// watchEffect(()=>{
+//     let flag = editableTabs.value.some((item: any) => item.name == route.path)
+//     if (!flag) {
+//         editableTabs.value.push({
+//             title: route.meta.title,
+//             name: route.path,
+//         })
+//     }
+// })
+onBeforeRouteUpdate((to, from) => { // 路由切换触发函数
+    console.log(to);
+    let flag = editableTabs.value.some((item: any) => item.name == to.path)
+        if (!flag) {
+            editableTabs.value.push({
+                title: to.meta.title,
+                name: to.path,
+            })
+        }
+})
+const once = (name: string) => {// 点击 跳转页面
+    console.log(name);
+    router.push({ path: name })
+}
+const handleTabsEdit = (val: any) => {// 删除事件
     console.log(val);
-    let res= editableTabs.value.filter((item:any)=>item.name!=val)
+    let res = editableTabs.value.filter((item: any) => item.name != val)
     console.log(res);
     editableTabs.value = res
 }
@@ -88,16 +102,21 @@ const Tui = () => { // 退出登录
 
 </script>
 <style lang='scss' scoped>
+.header-header{
+    border-bottom: 1px solid #ccc;
+}
+
 .m {
     :deep(.el-tabs__new-tab) {
         visibility: hidden;
     }
 
-    :deep(.el-tabs__item.is-active){
+    :deep(.el-tabs__item.is-active) {
         background-color: #409eff;
         color: #fff;
     }
-    :deep(.el-tabs__item){
+
+    :deep(.el-tabs__item) {
         padding: 0 15px !important;
     }
 }
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosError, type AxiosInstance, type AxiosRequestConfig, type AxiosResponse, type InternalAxiosRequestConfig, type Method } from "axios";
+import { ElLoading } from "element-plus";
 const service: AxiosInstance = axios.create({
     // baseURL: "/dev-api", // url = base url + request url
     baseURL: import.meta.env.VITE_APP_API_URL ,
@@ -7,6 +8,7 @@ const service: AxiosInstance = axios.create({
 // 拦截请求
 service.interceptors.request.use((config: InternalAxiosRequestConfig) => {
     // 请求头
+    ElLoading.service({ fullscreen: true,text:'疯狂加载中……' })
     const token = localStorage.getItem("token")
     if(token){
         config.headers.Authorization = "Bearer " + token
@@ -17,6 +19,9 @@ service.interceptors.request.use((config: InternalAxiosRequestConfig) => {
 })
 // 响应请求
 service.interceptors.response.use((response: AxiosResponse) => {
+    setTimeout(()=>{
+        ElLoading.service({ fullscreen: true,text:'疯狂加载中……' }).close()
+    },900)
         return response.data
     },(error:AxiosError)=>{
         return Promise.reject(error)
